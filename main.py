f55cdaf1a748495afa4c4946083eef03e3b33596
domvilla <dominiquevillarreal@Dominiques-MacBook-Air.local>
2023-01-17 22:11:06 -0700

    sample update

--- main.py
+++ main.py
@@ -6,19 +6,19 @@ print ("module 3 challenge")
 print ("Dominique Villarreal")
 
 
-budget_d = pd.read_csv("../PyBank/Resources/budget_data.csv")
+budget_d = os.path.join('..','PyBank','Resources', 'budget_data.csv')
 #print(budget_d)
 
 #total number of months
-total_months = len(budget_d['Date'].unique())
+total_months = len(budget_file['Date'].unique())
 
 
 #find profit/losses
 Profit = budget_d["Profit/Losses"].sum()
 
 #change in prof/loss
-change = budget_d["Profit/Losses"].diff(+1)
-budget_d["change"] = change
+Change = budget_d["Profit/Losses"].diff(+1)
+budget_d["Change"] = Change
 
 average_change = budget_d["change"].mean()
 average_change = round(int(average_change))
