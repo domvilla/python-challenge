4d5dfc72d25adcc7fca8365dadf4cbde038e290c
domvilla <dominiquevillarreal@Dominiques-Air.lan>
2023-01-24 11:16:42 -0600

    creating python challenge

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,10 +1,10 @@
+#import pandas as pd
 import pandas as pd
+
 import os
 import csv
 
 print ("module 3 challenge")
-print ("Dominique Villarreal")
-
 
 budget_d = pd.read_csv("../PyBank/Resources/budget_data.csv")
 #print(budget_d)
@@ -26,5 +26,27 @@ average_change = round(int(average_change))
 #greatest inc in profit
 greatinc = budget_d["change"].max()
 greatinc = round(int(greatinc))
+
+
+#DECEREASE
+decrease = budget_d["change"].min()
+decrease = round(int(greatest_decrease))
+
+
+#-------------------------------------------
+Print("financial analysis")
+Print("________________________")
+Print("Total Months: " + str(total_months))
+Print("Total: $" +str(Profit))
+Print("Average Change:$" +str(average_change))
+
+Print("____________________________")
+
+Filename = "../PyBank/
+File.write(Financial Analysis/n")
+File.write("_________"/n")
+File.write("Total Months: %d/n" % total_months)
+File.write(Total Revenue: $%/n" % profit)
+File.write(Average Change $%d/n" % average_change)
 
 
